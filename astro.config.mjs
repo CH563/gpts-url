164c34af4f6624647230c503d6b8337989447dc0
chenliwen <liwen.chen@mobvista.com>
2023-11-12 15:54:35 +0800

    icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
-    },
+      enabled: true
+    }
   }),
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
         container: {
@@ -11,5 +12,5 @@ export default {
         },
         extend: {},
     },
-	plugins: [],
+	plugins: [require('@tailwindcss/typography')],
 }
