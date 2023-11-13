0f54a8ff44e75a97cc98ddbe9f0abbe52242a4f6
chenliwen <liwen.chen@mobvista.com>
2023-11-13 17:27:04 +0800

    icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://gptsurl.com",
   output: "server",
   adapter: vercel({
     webAnalytics: {
