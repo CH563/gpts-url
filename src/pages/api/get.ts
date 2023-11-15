816954bd68569c1de0a3126d8797e579d6812cdc
chenliwen <liwen.chen@mobvista.com>
2023-11-15 11:37:46 +0800

    datas

--- src/pages/api/get.ts
+++ src/pages/api/get.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from 'astro';
 import client from '../../libs/postgres';
 
 export const GET: APIRoute = async () => {
-    const { rows } = await client.sql`SELECT url, title, icon, description, author FROM gpts_lists order by RANDOM() desc limit 360;`.catch(() => {
+    const { rows } = await client.sql`SELECT url, title, icon, description, author FROM gpts_lists order by RANDOM() desc limit 300;`.catch(() => {
         return {
             rows: []
         };
