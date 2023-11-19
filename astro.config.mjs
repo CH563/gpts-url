e0d6cdb05a1552d7c8924a556587d9cc6d16b5f1
chenliwen <liwen.chen@mobvista.com>
2023-11-19 15:11:11 +0800

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+
+const site = "https://gptsurl.com";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gptsurl.com",
+  site,
   output: "server",
   adapter: vercel({
     webAnalytics: {
       enabled: true
     }
   }),
-  integrations: [tailwind(), sitemap({
-    customPages: ['https://gptsurl.com/gpts-store/convertanything', 'https://gptsurl.com/gpts-store/drawn-to-style', 'https://gptsurl.com/gpts-store/logogpt', 'https://gptsurl.com/gpts-store/twUGxmpHv-visual-weather-artist-gpt', 'https://gptsurl.com/gpts-store/chatprd', 'https://gptsurl.com/gpts-store/universal-primer', 'https://gptsurl.com/gpts-store/nomadgpt', 'https://gptsurl.com/gpts-store/designergpt', 'https://gptsurl.com/gpts-store/aso-buddy']
-  }), react()]
+  integrations: [tailwind(), react()]
 });
